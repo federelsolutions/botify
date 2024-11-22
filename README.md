a44c56c04e1cc7db7c818741a153f6a81b66a295
dimitrihartt <dimitrileite@hotmail.com>
2024-11-22 12:43:34 -0300

    MillionRay logo

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Vue 3 + TypeScript + Vite
+# MillionRay App com IA
+
+This app has an AI that interacts with Real Estate Buyers and Sellers to close very nice deals!
+
+## Vue 3 + TypeScript + Vite
 
 This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    <a href="https://millionray.com" target="_blank">
+      <img src="./assets/MillionRay.png" class="logo" alt="MillionRay logo" />
+    </a>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,7 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Bem-vindos à MillionRay!" />
 </template>
 
 <style scoped>
